0a341c189031a209e3bad2daeefd5f7685de4a3d
Davide Areias <davideareias1@gmail.com>
2022-04-09 09:43:38 +0100

    store

--- src/components/Client/Event.js
+++ src/components/Client/Event.js
@@ -1,14 +1,37 @@
+import { useEffect, useState } from "react";
+
 const Event = ({ eventInfo }) => {
+  const [buyFood, setBuyFood] = useState(false);
+
   return (
-    <div className="font-bold text-2xl pt-10">
-      <h1>{eventInfo.title}</h1>
-      <div>
-        <img
-          className="flex justify-center mt-10 rounded-2xl"
-          src={eventInfo.photo}
-        ></img>
-      </div>
-      <h1 className="pt-5">{eventInfo.description}</h1>
+    <div>
+      {(() => {
+        if (buyFood === false) {
+          return (
+            <div className="font-bold text-2xl pt-10">
+              <h1>{eventInfo.title}</h1>
+              <div>
+                <img
+                  className="flex justify-center mt-10 rounded-2xl"
+                  src={eventInfo.photo}
+                ></img>
+              </div>
+              <h1 className="pt-5">{eventInfo.description}</h1>
+              <h1 onClick={() => setBuyFood(true)} className="pt-5">
+                Aceder á loja
+              </h1>
+            </div>
+          );
+        } else {
+          return (
+            <div className="flex justify-center text-center">
+              <div className="font-bold text-2xl pt-10">
+                Loja - {eventInfo.title}
+              </div>
+            </div>
+          );
+        }
+      })()}
     </div>
   );
 };
